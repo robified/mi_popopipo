0053fce192aee720e92d78e43c1130c56dc7801b
David Bae <baeddavid@gmail.com>
2019-09-13 14:13:57 -0500

    add models for posts and instruction for Robin on migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 POST_TYPE = (
     ('V', 'Vent'),
@@ -21,4 +22,9 @@ class Post(models.Model):
     views = models.IntegerField()
     flags = models.IntegerField()
     # Add User Auth to get user keys.
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # FOR ROBIN
+    # WHEN YOU RUN makemigrations IT WILL GIVE YOU A PROMPT
+    # TO SELECT EITHER OPTION 1 OR 2. SELECT OPTION 1 AND PRESS ENTER.
+    # IT WILL THEN ASK FOR A DEFAULT VALUE FOR OUR SUPER USER. HIS ID SHOULD
+    # BE 1 SINCE HE IS THE SUPER USER. PRESS 1 AND HIT ENTER.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
