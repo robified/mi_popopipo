42d0ade856e9820edb44fbdda53a1e9c650cf5a2
Robin <daorob@gmail.com>
2019-09-13 15:49:20 -0500

    Test file switch.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+ 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+ 
+def show(request):
+    return render(request, 'show.html')
+ 
+def new(request):
+    return render(request, 'new.html')
+ 
+def edit(request):
+    return render(request, 'edit.html')
+ 
+# def delete(request):
+#     return HttpResponse('<h1>Herro Edit /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+ 
+# def login(request):
+#     return HttpResponse('<h1>Herro Login /ᐠ｡‸｡ᐟ\ﾉ</h1>')
