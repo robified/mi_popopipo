a1b1d3c8e32e35c7803ad64a4447f54a7fb5dc6b
David Bae <baeddavid@gmail.com>
2019-09-13 14:46:58 -0500

    finished models

--- main_app/models.py
+++ main_app/models.py
@@ -19,12 +19,19 @@ class Post(models.Model):
     company_office_city = models.CharField(max_length=50)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    views = models.IntegerField()
-    flags = models.IntegerField()
-    # Add User Auth to get user keys.
+    views = 0
+    flags = 0
     # FOR ROBIN
     # WHEN YOU RUN makemigrations IT WILL GIVE YOU A PROMPT
     # TO SELECT EITHER OPTION 1 OR 2. SELECT OPTION 1 AND PRESS ENTER.
     # IT WILL THEN ASK FOR A DEFAULT VALUE FOR OUR SUPER USER. HIS ID SHOULD
     # BE 1 SINCE HE IS THE SUPER USER. PRESS 1 AND HIT ENTER.
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    body = models.TextField()
+    views = 0
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
