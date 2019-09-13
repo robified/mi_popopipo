24df839ba5665cadeb085e5c0e8f12fc10e687ac
David Bae <baeddavid@gmail.com>
2019-09-13 16:18:08 -0500

    update views import

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
 
 def home(request):
   return render(request, 'home.html')
