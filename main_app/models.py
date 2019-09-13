88970b0614fe34717d76f7bf313d8b4807f5c1e9
David Bae <baeddavid@gmail.com>
2019-09-13 12:11:09 -0500

    add Post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+POST_TYPE = (
+    ('V', 'Vent'),
+    ('I', 'Info'),
+    ('H', 'Help')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    categories = models.CharField(
+        max_length=1,
+        choices=POST_TYPE,
+        default=POST_TYPE[0][0]
+    )
+    company = models.CharField(max_length=50)
+    company_office_city = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_add_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    views = models.IntegerField()
+    flags = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
