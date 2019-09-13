41fd7bad8bb7e0608d0b5560fce64623c6e2c7df
Admin <Admin@MacBook-Pro-2.local>
2019-09-13 16:59:41 -0500

    added changes to urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
-from .import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup', views.signup, name='signup'),
+    path('show/', views.show, name='show'),
+    path('show/new', views.show.new, name='new'),
+    path('show/edit', views.show.edit, name='edit'),
+    path('login/', views.login, name='login'),
+
 ]
