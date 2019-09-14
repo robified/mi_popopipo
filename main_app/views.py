52fe390e4f4f33d6d190927319c2fa99971b0c3f
David Bae <baeddavid@gmail.com>
2019-09-14 13:58:58 -0500

    add views for comments

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django import forms
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -19,15 +20,53 @@ class PostDelete(DeleteView):
     model = Post
     success_url = '/'
 
+class CommentCreate(CreateView):
+  model = Comment
+  fields = ['body']
+  sucess_url = 'post/<int:post_id>/'
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Your Name"
+        })
+    )
+    body = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Leave a comment!"
+        })
+    )
+
 def home(request):
   return render(request, 'home.html')
 
 def post_index(request):
-  post = Post.objects.all()
+  post = Post.objects.all().order_by('-created_on')
   return render(request, 'blog/index.html', {'post': post})
 
 def post_detail(request, post_id):
   post = Post.objects.get(id=post_id)
+
+  form = CommentForm()
+  if request.method == 'POST':
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = Comment(
+        author=form.cleaned_data["author"],
+        body=form.cleaned_data["body"],
+        post=post
+      )
+      comment.save()
+
+  comments = Comment.objects.filter(post=post)
+  context = {
+    "post": post,
+    "comments": comments,
+    "form": form,
+  }
   return render(request, 'blog/detail.html', { 'post': post })
 
 def signup(request):
