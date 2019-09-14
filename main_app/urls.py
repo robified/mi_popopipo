d879053dbad23cf0f8bbe582f6a6ca2bbeb8a4f5
David Bae <baeddavid@gmail.com>
2019-09-13 20:37:52 -0500

    add url for new post

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('show/', views.show, name='show'),
-    path('show/new', views.show.new, name='new'),
-    path('show/edit', views.show.edit, name='edit'),
-    path('login/', views.login, name='login'),
-
+    # path('show/', views.show, name='show'),
+    # path('show/new', views.show.new, name='new'),
+    # path('show/edit', views.show.edit, name='edit'),
+    # path('login/', views.login, name='login'),
+    path('accounts/signup', views.signup, name='signup'),
+    path('new/', views.PostCreate.as_view(), name='post_create'),
 ]
