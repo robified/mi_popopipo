dad634b2928020d5b5b1ab56c9da6d4f2fe92a7e
David Bae <baeddavid@gmail.com>
2019-09-19 21:48:01 -0500

    update models adding a summary section

--- main_app/models.py
+++ main_app/models.py
@@ -32,6 +32,7 @@ class Post(VoteModel, models.Model):
     company = models.CharField(max_length=50)
     company_office_city = models.CharField(max_length=50)
     body = models.TextField()
+    summary = models.TextField(default='No Summary Was Found')
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     blog_views = models.IntegerField(default=0)
